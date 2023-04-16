1d0cbb230779e7a3b0fc2a1cfc78ab9ffc29f490
Jon Lamb <lamb.jon.io@gmail.com>
2023-04-16 03:40:48 -0700

    Rm temp bootloader project, lives in agp repo now

--- bootloader/src/tasks/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod net;
-pub mod watchdog;
-
-pub(crate) use self::net::{
-    eth_gpio_interrupt_handler_task, ipstack_clock_timer_task, ipstack_poll_task,
-    ipstack_poll_timer_task,
-};
-pub(crate) use self::watchdog::watchdog_task;
